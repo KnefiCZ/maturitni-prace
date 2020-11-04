f309b16f4e30418f0d606ddd18659d4724e4cbd8
KnefiCZ <matej.kneifl@seznam.cz>
2020-11-04 10:25:48 +0100

    -

---
+++ yolo/succesful_login.php
@@ -0,0 +1,5 @@
+<?php require 'session_details.php'; ?>
+<?php require '../header.php'; ?>
+      <h1>Sucessful login<?php echo 'User!'; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+<?php require '../footer.php'; ?>
--- yolo/user_login.php
+++ yolo/user_login.php
@@ -14,7 +14,7 @@
 
             if($count == 1) {
                 $_SESSION['email'] = "$email";
-                header("location: welcome.php");
+                header("location: succesful_login.php");
             }else {
                 $error = "Váš email nebo heslo nebylo zadáno správně!";
             }
@@ -23,6 +23,28 @@
     var_dump($password);
     var_dump($_SESSION);
 ?>
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>MATURITNÍ-PRÁCE</title>
+
+<!-- Custom fonts for this template -->
+<link href="../vendor/components/fontawesome-free/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+<!-- Custom styles for this page -->
+<link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+</head>
+
     <?php require_once "../header.php"; ?>
     <form action = "#" method = "post">
         <label for="email">Email:</label>
